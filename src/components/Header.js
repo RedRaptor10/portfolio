e504635dce038e9c849a8b14210459e69a840a3f
RedRaptor10 <weiminh_lam@yahoo.com>
2022-06-13 13:51:03 -0500

    Install FontAwesome icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faTools, faFolder, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
 const Header = () => {
     return (
         <header>
@@ -5,11 +8,11 @@ const Header = () => {
                 <h1 className="title">Weiminh Lam</h1>
             </a>
             <ul className="nav">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#skills">Skills</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li><a href="#home"><FontAwesomeIcon icon={faHome} />Home</a></li>
+                <li><a href="#about"><FontAwesomeIcon icon={faUser} />About</a></li>
+                <li><a href="#skills"><FontAwesomeIcon icon={faTools} />Skills</a></li>
+                <li><a href="#projects"><FontAwesomeIcon icon={faFolder} />Projects</a></li>
+                <li><a href="#contact"><FontAwesomeIcon icon={faEnvelope} />Contact</a></li>
             </ul>
         </header>
     );
