6b43e3f5d823e259a25522192759096a2147d323
RedRaptor10 <weiminh_lam@yahoo.com>
2022-07-01 14:25:49 -0500

    Add Particles background

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,56 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faAnglesDown } from '@fortawesome/free-solid-svg-icons';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
 
 const Home = () => {
+    const particlesInit = async (main) => {
+        await loadFull(main);
+    };
+
+    const particlesOptions = {
+        interactivity: {
+            events: {
+                onHover: {
+                    enable: true,
+                    mode: "grab"
+                }
+            },
+            modes: {
+                grab: {
+                    distance: 200,
+                    links: {
+                        color: "#80a0c0",
+                        opacity: 1
+                    }
+                }
+            }
+        },
+        particles: {
+            color: "#80a0c0",
+            links: {
+                color: "#80a0c0",
+                distance: 100,
+                enable: true,
+                opacity: 0.25,
+                width: 1
+            },
+            move: {
+                enable: true,
+                random: true,
+                speed: 2
+            },
+            opacity: {
+                value: 0.25
+            }
+        }
+    };
+
     return (
         <section id="home" className="transition hidden">
             <div className="home-header-container">
+                <Particles id="tsparticles" init={particlesInit} options={particlesOptions} />
                 <div className="home-header">
                     <h1>Weiminh Lam</h1>
                     <div className="about">
